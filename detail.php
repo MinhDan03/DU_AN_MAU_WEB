42a03e3f895fe294829033e5adc52070b0d81816
MinhDan03 <dan11102003net@gmail.com>
2023-01-04 23:42:53 +0700

    first commit

---
+++ detail.php
@@ -0,0 +1,7 @@
+<?php include_once('part/header.php')?>
+<div class="container-fluid">
+    <div class="row px-xl-5">
+        <?php include_once('components/detailcom.php')?>
+    </div>
+</div>
+<?php include_once('part/footer.php')?>
